95ca26dca15c6938f4d908741c9164a5c9f6039c
Zach Latta <zach@zachlatta.com>
2016-08-27 09:12:47 -0700

    Set up scheduling and configure for deployment

---
+++ Procfile
@@ -0,0 +1,1 @@
+clock: cd etl && bundle exec clockwork etl/config/clock.rb
---
+++ etl/config/clock.rb
@@ -0,0 +1,20 @@
+require_relative '../lib/common.rb'
+
+require 'clockwork'
+require 'kiba'
+
+module Clockwork
+  handler do |job|
+    relative_path = File.join('../jobs', job)
+    path = File.expand_path(relative_path, __dir__)
+
+    script_content = IO.read(path)
+    job_definition = Kiba.parse(script_content, path)
+
+    $logger.info "Running #{job}"
+    Kiba.run(job_definition)
+  end
+
+  every(1.day, 'sheets/slack_check_ins.etl')
+  every(15.minutes, 'streak/full_sync.etl')
+end
